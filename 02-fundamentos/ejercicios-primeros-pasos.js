ef8ec66b773703c6118416050cf0a9c945e7a66d
Elias <eliascur16@gmail.com>
2026-07-18 20:14:58 -0500

    Esta la tarea

--- 02-fundamentos/ejercicios-primeros-pasos.js
+++ 02-fundamentos/ejercicios-primeros-pasos.js
@@ -1,7 +1,43 @@
-//Comentario en una linea. 
-/*Comentario 
-  en 
-    varias 
-      lineas.*/
+// 1. Escribe un comentario en una línea
+//ya esta!
 
-//3. Declarar variables que tengan relacion a los datos primitivos. 
+// 2. Escribe un comentario en varias líneas
+/*
+  Xd
+  D(x)
+  La extraño
+  */
+
+// 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let string = ("Texto")  
+
+let int = (23)
+let double = (28.23)
+let float = (12.28282892)
+
+let bool = true
+let bool2 = false
+
+let undefinedZzz; //no esta definido.
+let nullValue; //no tieme valor. 
+
+let mySmy = symbol("Este seria el simbolp xd.")
+let zzBigInt = BigInt(128292937)
+let xxBigInt = (18393293838n)
+
+
+
+// 4. Imprime por consola el valor de todas las variables
+
+// 5. Imprime por consola el tipo de todas las variables
+
+// 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+
+// 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+
+// 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+
+// 9. A continuación, modifica los valores de las constantes
+
+// 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
+
